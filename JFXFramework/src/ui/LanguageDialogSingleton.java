ca5e4055e2717c3af5a2a9dde8ef544eb36cc1cd
eifu <etomita18@gmail.com>
2016-11-09 13:39:38 -0500

    new singleton dialog class for language choosing

--- JFXFramework/src/ui/LanguageDialogSingleton.java
+++ JFXFramework/src/ui/LanguageDialogSingleton.java
@@ -1,7 +1,130 @@
 package ui;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
 /**
- * Created by eifu on 11/9/16.
+ * @author Ritwik Banerjee
  */
-public class LanguageDialogSingleton {
+public class LanguageDialogSingleton extends Stage {
+    // HERE'S THE SINGLETON
+    static LanguageDialogSingleton singleton;
+
+    // GUI CONTROLS FOR OUR DIALOG
+    VBox   messagePane;
+    Scene  messageScene;
+    Label  messageLabel;
+    Button eButton;
+    Button jButton;
+    String selection;
+
+    // CONSTANT CHOICES
+    public static final String English    = "English";
+    public static final String Japanese     = "Japanese";
+
+    /**
+     * Note that the constructor is private since it follows
+     * the singleton design pattern.
+     */
+    private LanguageDialogSingleton() {}
+
+    /**
+     * The static accessor method for this singleton.
+     *
+     * @return The singleton object for this type.
+     */
+    public static LanguageDialogSingleton getSingleton() {
+        if (singleton == null)
+            singleton = new LanguageDialogSingleton();
+        return singleton;
+    }
+
+    /**
+     * This method initializes the singleton for use.
+     *
+     * @param primaryStage The window above which this dialog will be centered.
+     */
+    public void init(Stage primaryStage) {
+        // MAKE THIS DIALOG MODAL, MEANING OTHERS WILL WAIT
+        // FOR IT WHEN IT IS DISPLAYED
+        initModality(Modality.WINDOW_MODAL);
+        initOwner(primaryStage);
+
+        // LABEL TO DISPLAY THE CUSTOM MESSAGE
+        messageLabel = new Label();
+
+        // YES, NO, AND CANCEL BUTTONS
+        eButton = new Button(English);
+        jButton = new Button(Japanese);
+
+        // MAKE THE EVENT HANDLER FOR THESE BUTTONS
+        EventHandler<ActionEvent> yesNoCancelHandler = event -> {
+            LanguageDialogSingleton.this.selection = ((Button) event.getSource()).getText();
+            LanguageDialogSingleton.this.hide();
+        };
+
+
+        // AND THEN REGISTER THEM TO RESPOND TO INTERACTIONS
+        eButton.setOnAction(yesNoCancelHandler);
+        jButton.setOnAction(yesNoCancelHandler);
+
+        // NOW ORGANIZE OUR BUTTONS
+        HBox buttonBox = new HBox();
+        buttonBox.getChildren().add(eButton);
+        buttonBox.getChildren().add(jButton);
+
+        // WE'LL PUT EVERYTHING HERE
+        messagePane = new VBox();
+        messagePane.setAlignment(Pos.CENTER);
+        messagePane.getChildren().add(messageLabel);
+        messagePane.getChildren().add(buttonBox);
+
+        // MAKE IT LOOK NICE
+        messagePane.setPadding(new Insets(10, 20, 20, 20));
+        messagePane.setSpacing(10);
+
+        // AND PUT IT IN THE WINDOW
+        messageScene = new Scene(messagePane);
+        this.setScene(messageScene);
+    }
+
+    /**
+     * Accessor method for getting the selection the user made.
+     *
+     * @return Either YES, NO, or CANCEL, depending on which
+     * button the user selected when this dialog was presented.
+     */
+    public String getSelection() {
+        return selection;
+    }
+
+    /**
+     * This method loads a custom message into the label
+     * then pops open the dialog.
+     *
+     * @param title   The title to appear in the dialog window bar.
+     * @param message Message to appear inside the dialog.
+     */
+    public void show(String title, String message) {
+        // SET THE DIALOG TITLE BAR TITLE
+        setTitle(title);
+
+        // SET THE MESSAGE TO DISPLAY TO THE USER
+        messageLabel.setText(message);
+
+        // AND OPEN UP THIS DIALOG, MAKING SURE THE APPLICATION
+        // WAITS FOR IT TO BE RESOLVED BEFORE LETTING THE USER
+        // DO MORE WORK.
+        showAndWait();
+    }
+
 }
